2588b5f019be5120acac30efc3afa12484ba629a
Tony Chen <runcong@gmail.com>
2014-12-12 16:46:45 -0500

    removed unused variables

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('tamago_feed_render');
+        $treeBuilder->root('tamago_feed_render');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
